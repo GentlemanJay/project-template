c44b8fb468085e932ea4b5e4249595162ba6c4dd
XuShengjie <xsj1996@qq.com>
2022-10-16 17:12:16 +0800

    自定义异常处理类

--- src/main/java/com/example/projecttemplate/controller/TestController.java
+++ src/main/java/com/example/projecttemplate/controller/TestController.java
@@ -1,6 +1,7 @@
 package com.example.projecttemplate.controller;
 
 import com.example.projecttemplate.vo.Result;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,4 +19,13 @@ public class TestController {
         return Result.success("Hello");
     }
 
+
+    @ApiOperation("捕捉空指针异常")
+    @GetMapping("/testException")
+    public Result testException() {
+
+        int i = 10/0;
+
+        return Result.success("Hello");
+    }
 }
--- src/main/java/com/example/projecttemplate/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/com/example/projecttemplate/exception/handler/GlobalExceptionHandler.java
@@ -1,17 +1,10 @@
 package com.example.projecttemplate.exception.handler;
 
-import com.example.projecttemplate.enums.ExceptionEnum;
-import com.example.projecttemplate.enums.ResultEnum;
 import com.example.projecttemplate.exception.CustomException;
-import com.example.projecttemplate.exception.InternalException;
 import com.example.projecttemplate.vo.Result;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-
-import java.sql.SQLIntegrityConstraintViolationException;
 
 /**
  * 自定义全局异常
@@ -23,30 +16,20 @@ import java.sql.SQLIntegrityConstraintViolationException;
 public class GlobalExceptionHandler {
 
 	/**
-	 * 异常处理方法
+	 * 全局异常处理方法
 	 * @return
 	 */
-	@ExceptionHandler(SQLIntegrityConstraintViolationException.class)
-	@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-	public Result<String> exceptionHandler(SQLIntegrityConstraintViolationException ex){
+	@ExceptionHandler(Exception.class)
+	public Result<String> exceptionHandler(Exception ex){
 		log.error(ex.getMessage());
-
-		if(ex.getMessage().contains("Duplicate entry")){
-			return Result.error(ex.getMessage());
-		}
-
 		return Result.error(ex.getMessage());
 	}
 
-
-	@ExceptionHandler(InternalException.class)
-	public Result<String> handleInternalException(InternalException e)
-	{
-		log.error(e.getMessage(), e);
-		return Result.error(e.getMessage());
-	}
-
-
+	/**
+	 * 自定义处理异常
+	 * @param e
+	 * @return
+	 */
 	@ExceptionHandler(CustomException.class)
 	public Result<String> handleCustomException(CustomException e)
 	{
