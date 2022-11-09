358f26b77dd9b8cbf6e0fbb0203cd0e1609b7730
XuShengjie <xsj1996@qq.com>
2022-11-09 15:48:18 +0800

    spring参数校验

--- src/main/java/com/example/projecttemplate/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/com/example/projecttemplate/exception/handler/GlobalExceptionHandler.java
@@ -1,8 +1,10 @@
 package com.example.projecttemplate.exception.handler;
 
+import com.example.projecttemplate.enums.ResultEnum;
 import com.example.projecttemplate.exception.CustomException;
 import com.example.projecttemplate.vo.Result;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -25,14 +27,27 @@ public class GlobalExceptionHandler {
 		return Result.error(ex.getMessage());
 	}
 
+
+	/**
+	 * 参数校验异常拦截
+	 * controller层添加@Validated注解
+	 * @param e
+	 * @return
+	 */
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public Result<String> handleBindException(MethodArgumentNotValidException e) {
+		String defaultMessage = e.getBindingResult().getAllErrors().get(0).getDefaultMessage();
+		return Result.error(ResultEnum.ERROR.getCode(), defaultMessage);
+	}
+
+
 	/**
 	 * 自定义处理异常
 	 * @param e
 	 * @return
 	 */
 	@ExceptionHandler(CustomException.class)
-	public Result<String> handleCustomException(CustomException e)
-	{
+	public Result<String> handleCustomException(CustomException e) {
 		log.error(e.getMessage(), e);
 		return Result.error(e.getMessage());
 	}
