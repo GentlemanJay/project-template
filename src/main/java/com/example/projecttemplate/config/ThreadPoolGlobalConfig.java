36d47a92ebf47d05acfae64ce845a97334bbce7e
XuShengjie <xsj1996@qq.com>
2023-01-05 17:50:14 +0800

    新增全局线程池配置类

--- src/main/java/com/example/projecttemplate/config/ThreadPoolGlobalConfig.java
+++ src/main/java/com/example/projecttemplate/config/ThreadPoolGlobalConfig.java
@@ -17,19 +17,21 @@ public class ThreadPoolGlobalConfig {
     public ThreadPoolTaskExecutor defaultThreadPool() {
         ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
         //核心线程数目
-        executor.setCorePoolSize(5);
+        executor.setCorePoolSize(10);
         //指定最大线程数
         executor.setMaxPoolSize(50);
         //队列中最大的数目
-        executor.setQueueCapacity(100);
+        executor.setQueueCapacity(200);
+        //线程空闲后的最大存活时间
+        executor.setKeepAliveSeconds(60);
         //线程名称前缀
         executor.setThreadNamePrefix("DefaultThreadPool_");
         //rejection-policy：当pool已经达到max size的时候，如何处理新任务
         //CALLER_RUNS：不在新线程中执行任务，而是由调用者所在的线程来执行
-        //对拒绝task的处理策略
-        executor.setRejectedExecutionHandler(new ThreadPoolExecutor.AbortPolicy());
-        //线程空闲后的最大存活时间
-        executor.setKeepAliveSeconds(60);
+        //修改拒绝策略为使用当前线程执行
+        executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
+        executor.setWaitForTasksToCompleteOnShutdown(true);
+        executor.setAwaitTerminationSeconds(60);
         //加载
         executor.initialize();
 
