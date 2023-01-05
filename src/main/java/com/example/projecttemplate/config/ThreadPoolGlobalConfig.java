25da2511395689a3e09deb01d548cc3c0ca35ff0
XuShengjie <xsj1996@qq.com>
2023-01-05 17:27:19 +0800

    新增全局线程池配置类

---
+++ src/main/java/com/example/projecttemplate/config/ThreadPoolGlobalConfig.java
@@ -0,0 +1,38 @@
+package com.example.projecttemplate.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.ThreadPoolExecutor;
+
+/**
+ * 线程池配置类
+ * @author XuShengJie
+ * @date 2023/1/5 17:19
+ */
+@Configuration
+public class ThreadPoolGlobalConfig {
+    @Bean
+    public ThreadPoolTaskExecutor defaultThreadPool() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        //核心线程数目
+        executor.setCorePoolSize(5);
+        //指定最大线程数
+        executor.setMaxPoolSize(50);
+        //队列中最大的数目
+        executor.setQueueCapacity(100);
+        //线程名称前缀
+        executor.setThreadNamePrefix("DefaultThreadPool_");
+        //rejection-policy：当pool已经达到max size的时候，如何处理新任务
+        //CALLER_RUNS：不在新线程中执行任务，而是由调用者所在的线程来执行
+        //对拒绝task的处理策略
+        executor.setRejectedExecutionHandler(new ThreadPoolExecutor.AbortPolicy());
+        //线程空闲后的最大存活时间
+        executor.setKeepAliveSeconds(60);
+        //加载
+        executor.initialize();
+
+        return executor;
+    }
+}
